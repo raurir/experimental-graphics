6c2e868df6bbf825818ea6f09c065665444c2653
Rauri Rochford <raurir@gmail.com>
2015-02-04 21:36:25 +1100

    hexagon tiling reverted to brute force radius technique

--- js/hexagon_tile.js
+++ js/hexagon_tile.js
@@ -1,12 +1,22 @@
-var sw = 1000, sh = 1000;
-var radiusOuter = 35, radiusInner = 36, angle60 = 2 * Math.PI / 6;
+var sw = 800, 
+sh = 800,
+radiusOuter = 5 + Math.random() * 25,
+strokeSize = Math.random() * Math.random() * Math.random() * radiusOuter,
+radiusInner = radiusOuter - strokeSize + 0.3,
+smoothSize = 1 + Math.random() * 20,
+angle60 = 2 * Math.PI / 6;
 
 var graphics = dom.svg("svg", {width:sw, height:sh});
 document.body.appendChild(graphics);
 var scaler = dom.svg("g");
 graphics.appendChild(scaler);
 
-colours.setPalette(["#000044", "#000088", "#000033", "#000011", "#5CB9FC", "#ffffff"]);
+var neighbourGroups = dom.svg("g");
+graphics.appendChild(neighbourGroups);
+
+
+// colours.setPalette(["#000044", "#000088", "#000033", "#000011", "#5CB9FC", "#ffffff"]);
+// colours.setPalette(["#ff2244", "#ff3322"]);
 
 var points = [];
 for(var i = 0; i < 6; i++) {
@@ -19,22 +29,19 @@ var minHeight = radiusOuter * Math.sin(angle60); // this is edge to edge, not co
 
 var cols = Math.ceil(sw / radiusOuter / 3) + 1;
 var rows = Math.ceil(sh / minHeight) + 1;
-
-// cols -= 4;
-// rows -= 4
-
-
+// cols -= 3;
+// rows -= 6;
 var hexagons = cols * rows;
-con.log(rows,cols, hexagons);
+// con.log(rows,cols, hexagons);
 
 var hexs = [];
 
 for(var i = 0; i < hexagons; i++) {
 
   var row = Math.floor(i / cols);
-  var col = (i % cols);
+  var second = (row % 2 == 0);
+  var col = (i % cols) + (second ? 0.5 : 0);
   var x = col * radiusOuter * 3;
-  if (row % 2 == 0) x += radiusOuter * 3 / 2;
   var y = row * minHeight;
 
   // x += 100;
@@ -45,35 +52,12 @@ for(var i = 0; i < hexagons; i++) {
   scaler.appendChild(group);
 
 
-  // var something = cols * 2;
-
-  // var colour;
-  // if (row < 2) {
-  //   colour = colours.getRandomColour();
-  // } else {
-
-  //   var siblings = [];
-  //   if (col != 1) {
-  //     siblings.push(hexs[i - cols]);
-  //   }
-  //   if (col < cols - 1) {
-  //     siblings.push(hexs[i - (cols-1)]);
-  //   }
-  //   siblings.push(hexs[i - something]);
-
-  //   colour = colours.mixColours(siblings);
-  //   colour = colours.mutateColour(colour, 20);
-
-  //   // colour = colours.mutateColour(hexs[i - something], 50);
-  // }
-
-
-
   var hex = dom.svg("path", {
     "d": points.join(" ")
     // "style": { "fill": colour }
   });
   group.appendChild(hex);
+
 
   // var circle = dom.svg("circle", {
   //   "r": radiusInner * 0.5,
@@ -86,16 +70,67 @@ for(var i = 0; i < hexagons; i++) {
   //   "text-anchor": "middle",
   //   y: 5
   // });
-  // text.innerHTML = [i,row,col].join(":");
+  // text.innerHTML = i + "(" + col + "," + row + ")";
   // group.appendChild(text);
 
 
+  var neighbours = [];
+  if (row > 1) neighbours.push(i - 6); // T
+  if (second) {
+    if (col > 0) {
+      if (row > 1) neighbours.push(i - 3); // TL
+      if (row < rows - 1) neighbours.push(i + 3); // BL
+    }
+    if (col < cols - 1) {
+      if (row > 1) neighbours.push(i - 2); // TR
+      if (row < rows - 1) neighbours.push(i + 4); // BR
+    }
+  } else {
+    if (col > 0) {
+      if (row > 0.5) neighbours.push(i - 4); // TL
+      if (row < rows - 2) neighbours.push(i + 2); // BL
+    } 
+    if (col < cols) {
+      if (row > 0.5) neighbours.push(i - 3); // TR
+      if (row < rows - 2) neighbours.push(i + 3); // BR
+    }
+  }
+  if (row < rows - 2) neighbours.push(i + 6); // B
+
+  /*
+  group.index = i;
+  group.addEventListener("mouseover", function() {
+    con.log(this.index, hexs[this.index].neighbours);
+    var neighbours = hexs[this.index].neighbours;
+    for (var j = 0; j < neighbours.length; j++) {
+      var neighbour = hexs[neighbours[j]];
+      var circle = dom.svg("circle", {
+        "transform": "translate(" + neighbour.x + "," + neighbour.y + ")",
+        // x: neighbour.x,
+        // y: neighbour.y,
+        "r": radiusInner * 1,
+        "style": { "fill": "black" }
+      });
+      neighbourGroups.appendChild(circle);
+    };
+  });
+
+  group.addEventListener("mouseout", function() {
+    var svg = neighbourGroups;
+    while (svg.lastChild) {
+      svg.removeChild(svg.lastChild);
+    }
+  });
+  */
+
   hexs[i] = {
+    index: i,
     path: hex,
     x: x,
     y: y,
     colour: null,
-    rendered: false
+    rendered: false,
+    neighbours: neighbours
   };
 
 }
@@ -106,72 +141,111 @@ function shuffle(o){
     return o;
 };
 
-var randomHexes = shuffle(hexs.slice())//.sort(function(){ Math.random() > 0.5 ? -1 : 1});
+var randomHexes = shuffle(hexs.slice());
 
-var h = 0;
-function render() {
-  // var scale = 1;
-  // var rotate = 0;
-  // scaler.setAttribute("transform", "translate(400,400) rotate(" + rotate + ") scale(" + scale + ")");
-  // scaler.setAttribute("transform", "translate(400,400) rotate(" + rotate + ") scale(" + scale + ")");
-  //
+// var scale = 1;
+// var rotate = 0;
+// scaler.setAttribute("transform", "translate(400,400) rotate(" + rotate + ") scale(" + scale + ")");
+// scaler.setAttribute("transform", "translate(400,400) rotate(" + rotate + ") scale(" + scale + ")");
+//
+
+// con.log(hexagons);
+
+for(var h = 0; h < hexagons; h++) {
+
+  var index = randomHexes[h].index;
+  var item = hexs[index];
 
 
-  // for(var i = 0; i < randomHexes.length; i++) {
-    // var item = randomHexes[i];
-    // item.path.setAttribute("style", "fill:" + colours.getNextColour());
+  // var neighbours = [];
+  // for(var i = 0; i < item.neighbours.length; i++) {
+  //   var otherItemIndex = item.neighbours[i];
+  //   // con.log(otherItemIndex);
+  //   var otherItem = hexs[otherItemIndex];
+  //   if (otherItem.rendered) {
+  //     neighbours.push(otherItem.colour);
+  //   }
   // }
 
-  if (h < hexagons) {
-    var item = randomHexes[h];
 
-    var close = [];
-    for(var i = 0; i < hexagons; i++) {
-      if (i != h) {
-        var otherItem = randomHexes[i];
-        // con.log(otherItem)
-        if (otherItem.rendered) {
+  var close = [];
+  for(var i = 0; i < hexagons; i++) {
+    if (i != h) {
+      var otherItem = randomHexes[i];
+      // con.log(otherItem)
+      if (otherItem.rendered) {
 
-          var dx = item.x - otherItem.x,
-            dy = item.y - otherItem.y,
-            d = Math.sqrt(dx * dx + dy * dy);
+        var dx = item.x - otherItem.x,
+          dy = item.y - otherItem.y,
+          d = Math.sqrt(dx * dx + dy * dy);
 
-            // con.log(item.x, otherItem.x)
+          // con.log(item.x, otherItem.x)
 
-            if (d < radiusOuter * 2) {
-              close.push(otherItem.colour);
-            }
+          if (d < radiusOuter * smoothSize) {
+            close.push(otherItem.colour);
+          }
 
-        }
       }
     }
-
-
-
-    var colour;
-    if (close.length > 0 ) {
-      // colour = colours.getNextColour();
-
-      con.log("close", close.length)
-
-      colour = colours.mixColours(close);
-      colour = colours.mutateColour(colour, 30);
-
-    } else {
-      colour = colours.getNextColour();
-    }
-
-
-
-    item.path.setAttribute("style", "fill:" + colour);
-
-    randomHexes[h].rendered = true;
-    randomHexes[h].colour = colour;
-
-    h++;
-    requestAnimationFrame(render);
   }
 
 
+
+
+
+
+
+  // con.log(neighbours.length,close.length);
+  var colour;
+  if (close.length > 0 ) {
+    colour = colours.mixColours(close);
+    // colour = colours.mutateColour(colour, 3);
+  } else {
+    colour = colours.getNextColour();
+  }
+
+
+  item.path.setAttribute("style", "fill:" + colour);
+
+  hexs[index].rendered = true;
+  hexs[index].colour = colour;
+
 }
-render();
+
+
+
+
+/*
+
+for(var h = 0; h < hexagons; h++) {
+  var item = randomHexes[h];
+
+  var close = [];
+  // con.log(item.neighbours);
+  for(var i = 0; i < item.neighbours.length; i++) {
+    var otherItem = hexs[item.neighbours[i]];
+    // con.log(otherItem);
+    if (otherItem.rendered) {
+      close.push(otherItem.colour);
+    }
+  }
+
+  con.log(close);
+
+  var colour;
+  if (close.length > 0 ) {
+    colour = colours.mixColours(close);
+    colour = colours.mutateColour(colour, 10);
+  } else {
+    colour = colours.getNextColour();
+  }
+
+  item.path.setAttribute("style", "fill:" + colour);
+
+  randomHexes[h].rendered = true;
+  randomHexes[h].colour = colour;
+}
+
+*/
+
+
