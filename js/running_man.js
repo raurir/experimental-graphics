c59ce92d13e3bd36c0f738734b50485709184ba1
Rauri Rochford <raurir@gmail.com>
2015-06-14 22:33:27 +1000

    arms working

--- js/running_man.js
+++ js/running_man.js
@@ -2,6 +2,12 @@ var con = console;
 var isNode = (typeof module !== 'undefined');
 
 var limbs = {
+	"hips": {
+		"range": 0,
+		"baserot": 0,
+		"length": 0,
+		"offset": 0
+	},
 	"thigh": {
 		"range": 1,
 		"baserot": 0.3,
@@ -12,12 +18,24 @@ var limbs = {
 		"range": -0.6,
 		"baserot": -0.9,
 		"length": 90,
-		"offset": Math.PI / 2
+		"offset": 1.6
 	},
 	"foot": {
 		"range": 0.5,
 		"baserot": 1.4,
-		"length": 20,
+		"length": 25,
+		"offset": 0
+	},
+	"bicep": {
+		"range": 1.5,
+		"baserot": 0,
+		"length": 50,
+		"offset": 0
+	},
+	"forearm": {
+		"range": 1,
+		"baserot": 2,
+		"length": 40,
 		"offset": 0
 	}
 };
@@ -45,7 +63,7 @@ var running_man = (function() {
 			calc: function(time) {
 				var osc = options.movement.baserot + Math.sin(time + options.movement.offset + options.phase) * options.movement.range;
 				var pos = this.position(osc);
-				return Math.max(cy + pos.sy, cy + pos.ey);
+				return Math.max(pos.sy, pos.ey);
 			},
 			position: function(osc) {
 				if (osc) {
@@ -66,18 +84,18 @@ var running_man = (function() {
 				}
 				return this.pos;
 			},
-			render: function(max) {
+			render: function(x, y) {
 				ctx.beginPath();
 				ctx.strokeStyle = "#0f0";
 				ctx.fillStyle = "#0f0";
-				ctx.moveTo(cx + this.pos.sx, max + cy + this.pos.sy);
-				ctx.lineTo(cx + this.pos.ex, max + cy + this.pos.ey);
+				ctx.moveTo(x + this.pos.sx, y + this.pos.sy);
+				ctx.lineTo(x + this.pos.ex, y + this.pos.ey);
 				ctx.stroke();
 				ctx.closePath();
 
 				ctx.beginPath();
-				ctx.drawCircle(cx + this.pos.sx, max + cy + this.pos.sy, 5);
-				ctx.drawCircle(cx + this.pos.ex, max + cy + this.pos.ey, 5);
+				ctx.drawCircle(x + this.pos.sx, y + this.pos.sy, 5);
+				ctx.drawCircle(x + this.pos.ex, y + this.pos.ey, 5);
 				ctx.closePath();
 				ctx.fill();
 			}
@@ -88,7 +106,7 @@ var running_man = (function() {
 		output.innerHTML = ("var limbs = " + JSON.stringify(limbs, null, "\t") + ";");
 	};
 	settings();
-	
+
 
 	function createEditor(l,k) {
 		var edit = dom.element("div");
@@ -111,6 +129,9 @@ var running_man = (function() {
 	document.body.appendChild(editor);
 	editor.appendChild(output);
 
+	var hips = createLimb({parent: null, movement: limbs.hips, phase: 0});
+
+
 
 	var t1 = createLimb({parent: null, movement: limbs.thigh, phase: 0});
 	var c1 = createLimb({parent: t1, movement: limbs.calf, phase: 0});
@@ -119,16 +140,24 @@ var running_man = (function() {
 	var c2 = createLimb({parent: t2, movement: limbs.calf, phase: Math.PI});
 	var f2 = createLimb({parent: c2, movement: limbs.foot, phase: Math.PI});
 
+	var bicep1 = createLimb({parent: null, movement: limbs.bicep, phase: 0});
+	var forearm1 = createLimb({parent: bicep1, movement: limbs.forearm, phase: 0});
+	var bicep2 = createLimb({parent: null, movement: limbs.bicep, phase: Math.PI});
+	var forearm2 = createLimb({parent: bicep2, movement: limbs.forearm, phase: Math.PI});
+
 	function render(t) {
 		// var time = 50;
-		var time = t / 300;
+		var time = t / 250;
 
 		ctx.clearRect(0, 0, sw, sh);
 		// ctx.fillStyle = "#0f0";
 		// ctx.fillRect(cx - 2, cy - 2, 4, 4);
 
 		// cy = 200 - Math.abs(Math.sin(time) * 50);
-		var max = 0;
+
+		var max = 0; // calculate impact with ground, ie maximum y position. 
+		// we can hope it's either the end of the calf or the end of the foot. walking on knees is not currently accepted.
+		hips.calc(time);
 		t1.calc(time);
 		max = Math.max(max, c1.calc(time));
 		max = Math.max(max, f1.calc(time));
@@ -136,21 +165,28 @@ var running_man = (function() {
 		max = Math.max(max, c2.calc(time));
 		max = Math.max(max, f2.calc(time));
 
-		var horizon = 300;
+		bicep1.calc(time);
+		forearm1.calc(time);
+		bicep2.calc(time);
+		forearm2.calc(time);
+
+		var horizon = 400;
 		// cy = max;
 		ctx.fillStyle = "#040";
 		ctx.fillRect(cx - 100, horizon, 200, 10);
 
-		max = horizon - max;
-		t1.render(max);
-		c1.render(max);
-		f1.render(max);
-		t2.render(max);
-		c2.render(max);
-		f2.render(max);
-
-
-
+		var x = cx, y = horizon - max;
+		t1.render(x, y);
+		c1.render(x, y);
+		f1.render(x, y);
+		t2.render(x, y);
+		c2.render(x, y);
+		f2.render(x, y);
+		y -= 90;
+		bicep1.render(x, y);
+		forearm1.render(x, y);
+		bicep2.render(x, y);
+		forearm2.render(x, y);
 
 		requestAnimationFrame(render);
 	}
