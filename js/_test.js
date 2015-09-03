d438bc80cb836d310810dd9dda27f61fd2329c0e
raurir <raurir@gmail.com>
2015-09-03 10:52:23 +1000

    testing dom

--- js/_test.js
+++ js/_test.js
@@ -1,6 +1,27 @@
-
 define(function (require) {
 
+  var stage = dom.element("div");
+
+  var b1 = dom.button("test1", {style:{color:"white"}});
+  var b2 = dom.button("test2", {style:{color:"white"}});
+
+  stage.appendChild(b1);
+  stage.appendChild(b2);
+
+  dom.on(b1, ["click"], go);
+
+
+  function go() {
+    con.log("go!!")
+    dom.on(b2, ["click"], go);
+    dom.off(b1, ["click"], go);
+  }
+
 	con.log('test loaded');
-	return 1;
+	// return 1;
+  return {
+    stage: stage,
+    init: function() {},
+    resize: function() {},
+  }
 });
--- js/dom.js
+++ js/dom.js
@@ -22,104 +22,132 @@ if (isNode) {
 
 var dom = (function() {
 
-	function setProps(el, props) {
-		for (var p in props) {
-			if (p == "style") {
-				for (var s in props[p]) {
-					el[p][s] = props[p][s];
-				}
-			} else {
-				el[p] = props[p];
-			}
-		}
-		return el;
-	}
+  function setProps(el, props) {
+    for (var p in props) {
+      if (p == "style") {
+        for (var s in props[p]) {
+          el[p][s] = props[p][s];
+        }
+      } else {
+        el[p] = props[p];
+      }
+    }
+    return el;
+  }
 
-	function setAttributes(el, attrs) {
-		for (var p in attrs) {
-			el.setAttribute(p,attrs[p]);
-		}
-		return el;
-	}
+  function setAttributes(el, attrs) {
+    for (var p in attrs) {
+      el.setAttribute(p,attrs[p]);
+    }
+    return el;
+  }
 
-	function element(element, props) {
-		var el = document.createElement(element);
-		setProps(el, props);
-		el.setSize = function(w,h) {
-			el.style.width = w + "px"; // i always use pixels... don't you?
-			el.style.height = h + "px";
-		};
-		// el.addClass = function(className) {
-		// 	// el.classList.add(className);
-		// }
-		// el.removeClass = function(class) {
-		// 	// el.classList.remove(class);
-		// }
+  function element(element, props) {
+    var el = document.createElement(element);
+    setProps(el, props);
+    el.setSize = function(w,h) {
+      el.style.width = w + "px"; // i always use pixels... don't you?
+      el.style.height = h + "px";
+    };
+    // el.addClass = function(className) {
+    //  // el.classList.add(className);
+    // }
+    // el.removeClass = function(class) {
+    //  // el.classList.remove(class);
+    // }
 
-		return el;
-	}
+    return el;
+  }
 
-	function canvas(w, h) {
+  function canvas(w, h) {
 
-		var c;
-		if (isNode) {
-			c = new Canvas(w,h);
-		} else {
-			c = element("canvas");
-			c.width = w;
-			c.height = h;
-		}
+    var c;
+    if (isNode) {
+      c = new Canvas(w,h);
+    } else {
+      c = element("canvas");
+      c.width = w;
+      c.height = h;
+    }
 
-		var ctx = c.getContext("2d");
+    var ctx = c.getContext("2d");
 
-		var circleRads = Math.PI * 2;
-		ctx.drawCircle = function(x, y, r) {
-			ctx.arc(x, y, r, 0, circleRads, false);
-		}
+    var circleRads = Math.PI * 2;
+    ctx.drawCircle = function(x, y, r) {
+      ctx.arc(x, y, r, 0, circleRads, false);
+    }
 
-		var setSize = function(w, h, preserveCanvas) {
-			if (preserveCanvas) {
-				c.setSize(w, h);
-			} else {
-				c.width = w;
-				c.height = h;
-			}
-		}
+    var setSize = function(w, h, preserveCanvas) {
+      if (preserveCanvas) {
+        c.setSize(w, h);
+      } else {
+        c.width = w;
+        c.height = h;
+      }
+    }
 
-		return {
-			setSize: setSize,
-			canvas: c,
-			ctx: ctx
-		}
-	}
+    return {
+      setSize: setSize,
+      canvas: c,
+      ctx: ctx
+    }
+  }
 
-	function button(txt, props) {
-		props.innerHTML = txt;
-		var b = element("div", props);
-		return b;
-	}
-
-
-	function svg(tag, props) {
-		var el = document.createElementNS("http://www.w3.org/2000/svg", tag);
-		setAttributes(el, props);
-		el.setSize = function(w,h) {
-			el.setAttribute("width", w);
-			el.setAttribute("height", h);
-		};
-		return el;
-	}
+  function button(txt, props) {
+    props = props || {};
+    if (props.innerHTML) con.warn("Specify butotn text as 1st param");
+    props.innerHTML = txt;
+    var b = element("div", props);
+    return b;
+  }
 
 
+  function svg(tag, props) {
+    var el = document.createElementNS("http://www.w3.org/2000/svg", tag);
+    setAttributes(el, props);
+    el.setSize = function(w,h) {
+      el.setAttribute("width", w);
+      el.setAttribute("height", h);
+    };
+    return el;
+  }
 
 
 
-	return {
-		element: element,
-		canvas: canvas,
-		button: button,
-		svg: svg
-	}
+
+
+
+
+
+
+  function on(target, events, callback) {
+    for (var i = 0, il = events.length; i < il; i++) {
+      target.addEventListener(events[i], callback);
+    }
+  }
+  function off(target, events, callback) {
+    for (var i = 0, il = events.length; i < il; i++) {
+      target.removeEventListener(events[i], callback);
+    }
+  }
+  function trigger(target, events) {
+    for (var i = 0, il = events.length; i < il; i++) {
+      target.dispatchEvent(events[i]);
+    }
+  }
+
+
+
+
+  return {
+    element: element,
+    canvas: canvas,
+    button: button,
+    svg: svg,
+    on: on,
+    off: off,
+    trigger: trigger
+  }
 
 
 })();
