7b9d7df044960bbe74f133da3486cf042bc869b0
raurir <raurir@gmail.com>
2016-06-10 13:38:31 +1000

    recursed

--- js/circle_packing.js
+++ js/circle_packing.js
@@ -6,48 +6,84 @@ var circle_packing = function() {
 
 	var bmp = dom.canvas(sw, sh);
 
-	var cx = sw / 2;
-	var cy = sh / 2;
-
 	var experiment = {
 		stage: bmp.canvas,
-		inner: null,
-		resize: function() {},
-		init: function() {},
-		kill: function() {},
+		init: init,
 		settings: {} // or null
 	}
-	bmp.ctx.clearRect(0, 0, sw, sh);
 
-	var iterations = 0;
-	function drawCircle(parent) {
-		var x = rand.random();
-		var y = rand.random();
-		var r = rand.random();
-		bmp.ctx.beginPath();
-		bmp.ctx.fillStyle = parent.depth % 2 ? "#010" : "#040";
-		bmp.ctx.drawCircle(x * sw, y * sh, r * 20);
-		bmp.ctx.closePath();
-		bmp.ctx.fill();
-		iterations++;
-		var circle = {
-			depth: parent.depth + 1,
-			x: x,
-			y: y,
-			r: r
-		}
-		if (iterations < 20) {
-			var num = rand.random() * 3;
-			for (var i = 0; i < num; i++) {
-				drawCircle(circle);
+	function init() {
+		bmp.ctx.clearRect(0, 0, sw, sh);
+
+		var iterations = 0;
+		function drawCircle(parent) {
+
+			var x, y, r, depth, colour;
+			if (parent){
+				var angle = rand.random() * Math.PI * 2;
+				var distance = rand.random() * parent.r; // distance from centre of parent
+				x = parent.x + Math.sin(angle) * distance;
+				y = parent.y + Math.cos(angle) * distance;
+				r = rand.random() * (parent.r - distance);
+				depth = parent.depth + 1;
+
+				var ok = true;
+				for (var i = 0, il = parent.children.length; i < il && ok; i++) {
+					var dx = x - parent.x,
+						dy = y - parent.y,
+						d = Math.sqrt(dx * dx + dy * dy), // minimum required distance between centres
+						dR = r + parent.r; // actual distance
+
+					if (dR < d) {
+						ok = false;
+					}
+				}
+				if (ok === false) {
+					con.log("red");
+					colour = "red" ;
+					// return;
+				} else {
+					colour = depth % 2 == 0 ? "#010" : "#040";
+				}
+
+			} else {
+				x = 0.5;//rand.random();
+				y = 0.5;//rand.random();
+				r = rand.random() / 2;
+				depth = 0;
+			}
+
+			bmp.ctx.beginPath();
+			bmp.ctx.fillStyle = colour;
+			bmp.ctx.drawCircle(x * sw, y * sh, r * sw);
+			bmp.ctx.closePath();
+			bmp.ctx.fill();
+
+			// con.log("iterations", iterations, x, y, r, bmp.ctx.fillStyle, depth, depth % 2);
+
+			iterations++;
+			var circle = {
+				depth: depth,
+				x: x,
+				y: y,
+				r: r,
+				children: []
+			}
+
+			if (parent && parent.children) parent.children.push(circle);
+
+			if (iterations < 500) {
+				var num = rand.random() * 6;
+				for (var i = 0; i < num; i++) {
+					drawCircle(circle);
+				}
 			}
 		}
+
+		drawCircle(0);
+
+		progress("render:complete", bmp.canvas);
 	}
-
-	drawCircle(0);
-
-	progress("render:complete", bmp.canvas);
-
 	return experiment;
 
 };
