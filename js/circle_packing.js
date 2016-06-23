4d8e45302636e62a315ad8dcb1a7e94ea5a4b9c1
Rauri Rochford <raurir@gmail.com>
2016-06-23 22:06:10 +1000

    circle pack

--- js/circle_packing.js
+++ js/circle_packing.js
@@ -12,11 +12,24 @@ var circle_packing = function() {
 		settings: {} // or null
 	}
 
+
+	function drawLine(p0, p1, colour, lineWidth) {
+		bmp.ctx.strokeStyle = colour;
+		bmp.ctx.lineWidth = lineWidth;
+		bmp.ctx.beginPath();
+		bmp.ctx.moveTo(p0.x, p0.y);
+		bmp.ctx.lineTo(p1.x, p1.y);
+		bmp.ctx.stroke();
+	}
+
 	function init() {
 		bmp.ctx.clearRect(0, 0, sw, sh);
 
 		var iterations = 0;
-		function drawCircle(parent) {
+		var circles = 0;
+		function drawCircle(parent, attempt) {
+
+			var gap = 0.01;
 
 			var x, y, r, depth, colour;
 			if (parent){
@@ -24,32 +37,50 @@ var circle_packing = function() {
 				var distance = rand.random() * parent.r; // distance from centre of parent
 				x = parent.x + Math.sin(angle) * distance;
 				y = parent.y + Math.cos(angle) * distance;
-				r = rand.random() * (parent.r - distance);
+				r = rand.random() * (parent.r - distance - gap);
+				// r = parent.r - distance - gap;
+
+				if (r < 0.001) {
+					if (attempt < 1000) {
+						drawCircle(parent, attempt + 1);
+					}
+					return;
+				}
+
 				depth = parent.depth + 1;
 
 				var ok = true;
 				for (var i = 0, il = parent.children.length; i < il && ok; i++) {
-					var dx = x - parent.x,
-						dy = y - parent.y,
+					var other = parent.children[i]
+					var dx = x - other.x,
+						dy = y - other.y,
 						d = Math.sqrt(dx * dx + dy * dy), // minimum required distance between centres
-						dR = r + parent.r; // actual distance
+						dR = r + other.r + gap; // actual distance
+						// con.log(parent.children, dR, d);
 
-					if (dR < d) {
+						// drawLine({x: other.x * sw, y: other.y * sw}, {x:x*sw, y:y*sw}, "red", 2);
+
+					if (dR > d) {
 						ok = false;
 					}
 				}
 				if (ok === false) {
-					con.log("red");
-					colour = "red" ;
-					// return;
+					// con.log("red");
+					colour = "rgba(255, 0, 0, 0.5)";
+					if (attempt < 1000) {
+						drawCircle(parent, attempt + 1);
+					}
+					return;
 				} else {
-					colour = depth % 2 == 0 ? "#010" : "#040";
+					// colour = depth % 2 == 0 ? "rgba(0, 0, 255, 0.5)" : "rgba(0, 255, 0, 0.5)";
+					colour = depth % 2 == 0 ? "black" : "white";
 				}
 
 			} else {
 				x = 0.5;//rand.random();
 				y = 0.5;//rand.random();
-				r = rand.random() / 2;
+				r = 0.5;//rand.random() / 2;
+				colour = "transparent";
 				depth = 0;
 			}
 
@@ -58,6 +89,12 @@ var circle_packing = function() {
 			bmp.ctx.drawCircle(x * sw, y * sh, r * sw);
 			bmp.ctx.closePath();
 			bmp.ctx.fill();
+
+
+			if (ok === false) {
+				return;
+			}
+
 
 			// con.log("iterations", iterations, x, y, r, bmp.ctx.fillStyle, depth, depth % 2);
 
@@ -70,17 +107,34 @@ var circle_packing = function() {
 				children: []
 			}
 
+			circles++;
 			if (parent && parent.children) parent.children.push(circle);
 
-			if (iterations < 500) {
-				var num = rand.random() * 6;
-				for (var i = 0; i < num; i++) {
-					drawCircle(circle);
+			// if (iterations < 4) {//500) {
+			setTimeout(function () {
+				if (depth < 2) {
+					var num = 500;//rand.random() * 6;
+					for (var i = 0; i < num; i++) {
+						drawCircle(circle, 0);
+					}
+					// setTimeout(function () {
+					// 	drawCircle(circle, 0);
+					// }, 1);
 				}
+			}, 1);
+
+			if (parent && attempt < 1500) {
+				con.log("doing", attempt, circles);
+				setTimeout(function () {
+					drawCircle(parent, attempt + 1);
+				}, 1);
 			}
+
+
+			return circle;
 		}
 
-		drawCircle(0);
+		drawCircle(0, 0);
 
 		progress("render:complete", bmp.canvas);
 	}
