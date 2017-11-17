40d9e5a373b9c30dc9727ca378d6ecdacdcb7969
Rauri Rochford <raurir@gmail.com>
2017-11-17 20:58:01 +1100

    fail sphere

--- js/yes_day.js
+++ js/yes_day.js
@@ -1,76 +1,95 @@
 define("yes_day", [
-  "lib/schteppe/cannon.0.6.2.min.js",
-  "lib/schteppe/cannon.demo.js",
-  "http://schteppe.github.io/cannon.js/libs/TrackballControls.js",
-  ], function() {
+	"lib/schteppe/cannon.0.6.2.min.js",
+	"lib/schteppe/cannon.demo.js",
+	"http://schteppe.github.io/cannon.js/libs/TrackballControls.js",
+	], function() {
 
-  var demo = new CANNON.Demo();
+	var demo = new CANNON.Demo();
 
+	demo.addScene("ball-and-chain",function(){
+			var world = setupWorld(demo);
+			world.gravity.set(0, 0, -20);
+			var width = 1;
+			var wireSize = 0.3;
+			var pitch = 1;
+			var sphereShape = new CANNON.Sphere(width);
+			var chainShape0 = new CANNON.Box(new CANNON.Vec3(width, wireSize, pitch));
+			var chainShape1 = new CANNON.Box(new CANNON.Vec3(wireSize, width, pitch));
+			var mass = 1;
+			var space = 1;
+			var N = 2, last;
+			for(var i = 0; i < N; i++){
+				var first = i == 0;
 
-  // We link together boxes in a chain
-  demo.addScene("links",function(){
-      var world = setupWorld(demo);
-      world.gravity.set(0, 0, -20);
-      var size = 1;
-      var chainSize = 0.3;
-      var linkLength = size * 2;
-      var sphereShape = new CANNON.Sphere(size);
-      var chainShape0 = new CANNON.Box(new CANNON.Vec3(size, chainSize, linkLength));
-      var chainShape1 = new CANNON.Box(new CANNON.Vec3(chainSize, size, linkLength));
-      var mass = 1;
-      var space = 0.25;
-      var N = 20, last;
-      for(var i=0; i<N; i++){
-          // Create a box
-          var boxbody = new CANNON.Body({ mass: mass });
-          boxbody.addShape(i % 2 ? chainShape1 : chainShape0);
-          boxbody.position.set(0, 0, (N-i)*(linkLength*2+2*space) + linkLength*2+space);
-          // boxbody.quaternion.setFromAxisAngle(new CANNON.Vec3(0,1,0), Math.PI / 2);
-          boxbody.linearDamping = 0.1; // Damping makes the movement slow down with time
-          boxbody.angularDamping = 0.1;
-          boxbody.velocity.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+				var py = (N-i)*(pitch*2+2*space) + pitch*2+space;
 
-          world.addBody(boxbody);
-          demo.addVisual(boxbody);
-          if(i!=0){
-              // Connect the current body to the last one
-              // We connect two corner points to each other.
-              var cnX = size * 0.1, cnY = linkLength + space;
-              var c1 = new CANNON.PointToPointConstraint(boxbody, new CANNON.Vec3(-cnX, 0, cnY), last, new CANNON.Vec3(-cnX, 0, -cnY));
-              var c2 = new CANNON.PointToPointConstraint(boxbody, new CANNON.Vec3(cnX, 0, cnY), last, new CANNON.Vec3(cnX, 0, -cnY));
-              world.addConstraint(c1);
-              world.addConstraint(c2);
-          } else {
-              // First body is now static. The rest should be dynamic.
-              mass=0.3;
-          }
-          // To keep track of which body was added last
-          last = boxbody;
-      }
-  });
+				// Create a box
+				var chainLinkBody = new CANNON.Body({mass: 0.1});
+				chainLinkBody.addShape(i % 2 ? chainShape1 : chainShape0);
+				chainLinkBody.position.set(0, 0, py);
+				chainLinkBody.linearDamping = 0.1;
+				chainLinkBody.angularDamping = 0.1;
+				chainLinkBody.velocity.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
 
+				world.addBody(chainLinkBody);
 
-  function setupWorld(demo){
-    // Create world
-    var world = demo.getWorld();
-    world.gravity.set(0,0,-40);
-    world.broadphase = new CANNON.NaiveBroadphase();
-    world.solver.iterations = 10;
-    // ground plane
-    var groundShape = new CANNON.Plane();
-    var groundBody = new CANNON.Body({ mass: 0 });
-    groundBody.addShape(groundShape);
-    groundBody.position.set(0,0,1);
-    world.addBody(groundBody);
-    demo.addVisual(groundBody);
-    world.quatNormalizeFast = false;
-    world.quatNormalizeSkip = 0;
-    return world;
-  };
+				var spherebody = new CANNON.Body({mass: 0.1});
+        spherebody.addShape(sphereShape);
+        spherebody.position.set(0, 0, py);
+        // spherebody.velocity.x = i;
+        world.add(spherebody);
+        demo.addVisual(spherebody);
+
+				demo.addVisual(chainLinkBody);
+
+				var cnX = width * 0.1, cnY = pitch + space;
+
+				if(!first){
+					// Connect the current body to the last one
+					// We connect two corner points to each other.
+					var c1 = new CANNON.PointToPointConstraint(chainLinkBody, new CANNON.Vec3(-cnX, 0, cnY), last, new CANNON.Vec3(-cnX, 0, -cnY));
+					var c2 = new CANNON.PointToPointConstraint(chainLinkBody, new CANNON.Vec3(cnX, 0, cnY), last, new CANNON.Vec3(cnX, 0, -cnY));
+					// world.addConstraint(c = new CANNON.DistanceConstraint(spherebody,lastBody,dist));
+
+					world.addConstraint(c1);
+					world.addConstraint(c2);
+				}
+
+				var c3 = new CANNON.PointToPointConstraint(spherebody, new CANNON.Vec3(-cnX, 0, cnY), chainLinkBody, new CANNON.Vec3(-cnX, 0, -cnY));
+				var c4 = new CANNON.PointToPointConstraint(spherebody, new CANNON.Vec3(cnX, 0, cnY), chainLinkBody, new CANNON.Vec3(cnX, 0, -cnY));
+
+				world.addConstraint(c3);
+				world.addConstraint(c4);
 
 
 
+				// To keep track of which body was added last
+				last = spherebody;
+			}
+	});
 
-  demo.start();
+
+	function setupWorld(demo){
+		// Create world
+		var world = demo.getWorld();
+		world.gravity.set(0,0,-40);
+		world.broadphase = new CANNON.NaiveBroadphase();
+		world.solver.iterations = 10;
+		// ground plane
+		var groundShape = new CANNON.Plane();
+		var groundBody = new CANNON.Body({ mass: 0 });
+		groundBody.addShape(groundShape);
+		groundBody.position.set(0,0,1);
+		world.addBody(groundBody);
+		demo.addVisual(groundBody);
+		world.quatNormalizeFast = false;
+		world.quatNormalizeSkip = 0;
+		return world;
+	};
+
+
+
+
+	demo.start();
 
 });
