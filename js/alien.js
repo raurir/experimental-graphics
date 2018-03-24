852a335c975208a479ae5639e3d6bc897bd8f7e4
Rauri Rochford <raurir@gmail.com>
2018-03-24 13:24:24 +1100

    alien grid

--- js/alien.js
+++ js/alien.js
@@ -1,9 +1,23 @@
 var isNode = typeof module !== "undefined";
 
+if (isNode) {
+	var con = console;
+	var rand = require('./rand.js');
+	var dom = require('./dom.js');
+	var colours = require('./colours.js');
+}
+
 var alien = function() {
 	var stage = dom.canvas(1, 1);
 	var ctx = stage.ctx;
 	var centre;
+
+	const numberOfRows = 10;
+	const numberOfColumns = 10;
+	const cellSize = Math.ceil(rand.getInteger(20, 60) / numberOfColumns);
+	const halfColumns = numberOfColumns / 2;
+	const canvasWidth = cellSize * (numberOfColumns + 2);
+	const canvasHeight = cellSize * (numberOfRows + 2);
 
 	function init(options) {
 		size = options.size;
@@ -12,27 +26,27 @@ var alien = function() {
 		stage.setSize(sw, sh);
 		centre = sh / 2;
 		colours.getRandomPalette();
-		spokes = rand.getInteger(2, 40) * 2;
-
 		render();
 	}
 
 	function render() {
-		const img = alien();
-		ctx.drawImage(img, centre - img.width / 2, centre - img.height / 2);
+		const aliens = manyAliens();
+		con.log(aliens)
+		aliens.x.forEach((x, xi) => {
+			aliens.y.forEach((y) => {
+				const img = oneAlien();
+				ctx.drawImage(img, centre + x * canvasWidth, centre + y * canvasHeight);
+			});
+			progress("render:progress", xi / aliens.x.length);
+		});
+		progress("render:complete", stage.canvas);
 	}
 
-	function alien() {
+	function oneAlien() {
 		const grid = [];
 
-		const cellSize = 30;
-		const numberOfRows = 10;
-		const numberOfColumns = 10;
-
-		const halfColumns = numberOfColumns / 2;
-
-		const canvasWidth = cellSize * (numberOfColumns + 2);
-		const canvasHeight = cellSize * (numberOfRows + 2);
+		let lineSize = rand.getInteger(0, 3); // either draw no line
+		if (lineSize) lineSize = cellSize / lineSize; // or fraction of cell size
 
 		const bmp = dom.canvas(canvasWidth, canvasHeight);
 
@@ -57,7 +71,7 @@ var alien = function() {
 			const colourFill = colours.getNextColour();
 			for (row = 0; row < numberOfRows; row++) {
 				for (column = 0; column < numberOfColumns; column++) {
-					// drawCell(row, column, colourLine, 10);
+					drawCell(row, column, colourLine, lineSize);
 				}
 			}
 			for (row = 0; row < numberOfRows; row++) {
@@ -93,13 +107,26 @@ var alien = function() {
 		return bmp.canvas;
 	}
 
+	function manyAliens() {
+		function r( m ) { return ~~(Math.random() * m + 1)};
+		function q() {
+			var l = r(11), g = r(5), a = [], i = 0;
+			while ( i < l ) {
+				j = i * g;
+				a.push( j );
+				if ( i ) a.unshift(-j)
+				i++;
+			}
+			return a;
+		}
+	  return { x: q(), y: q() };
+	}
+
 	var experiment = {
 		init: init,
 		render: render,
 		stage: stage.canvas
 	};
-
-	progress("render:complete", stage.canvas);
 
 	return experiment;
 };
