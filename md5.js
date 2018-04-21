ac1175f7caaafa7e0af0cb56a76616fcc5484d90
Rauri Rochford <raurir@gmail.com>
2018-04-21 14:57:05 +1000

    log build hash

--- md5.js
+++ md5.js
@@ -1,4 +1,13 @@
-module.exports = () =>
-	require('crypto').createHash('md5').update(
+module.exports = () => {
+	const md5 = require('crypto').createHash('md5').update(
 		require('fs').readFileSync(`${__dirname}/jsmin/composite.js`)
-	).digest('hex')
+	).digest('hex');
+	// https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+	console.log(
+`\x1b[32m--------------------------------
+BUILD HASH
+${md5}
+--------------------------------\x1b[0m`
+	);
+	return md5;
+};
