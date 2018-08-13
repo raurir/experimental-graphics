1298c3ba5f927ab83d646ac3ca59d7cb18e07e61
Rauri Rochford <raurir@gmail.com>
2018-08-13 23:59:40 +1000

    remove self intersecting

--- src/reddit_proc_gen.js
+++ src/reddit_proc_gen.js
@@ -13,7 +13,8 @@ const reddit_proc_gen = function() {
 	let cutHalf;
 
 	// been listening to GOTO80 for most of this: https://www.youtube.com/watch?v=2ZXlofdWtWw
-	// and finishing up with tranan + hux flux
+	// followed by tranan + hux flux
+	// next session: somfay
 
 	// copied from recursive_polygon
 	const drawPolygon = (points, { lineWidth, strokeStyle, fillStyle }) => {
@@ -48,7 +49,7 @@ const reddit_proc_gen = function() {
 		const cornerMin = Math.min(cornerAlpha, cornerBeta);
 		const cornerMax = Math.max(cornerAlpha, cornerBeta);
 
-		// a edge is between point{K}0 - point{K}1
+		// an edge is between point{K}0 - point{K}1
 		const pointA0 = array[cornerMin];
 		const pointA1 = array[(cornerMin + 1) % array.length];
 		const pointB0 = array[cornerMax];
@@ -68,23 +69,30 @@ const reddit_proc_gen = function() {
 			cutHalf ? 0.5 : rand.getNumber(0.1, 0.9)
 		);
 
+		// min and max are confusing, but one poly starts from 0 which is arrayMin.
 		const arrayMin = [];
 		const arrayMax = [];
 
-		// add first polygon
+		// add first polygon - start from the first point of the parent
 		var i = 0;
+		// and navigate around the parent's perimeter
 		while (i < array.length) {
 			if (i <= cornerMin) {
 				arrayMin.push(array[i]);
+				//  until we hit the slice point
 			} else if (i > cornerMin && i <= cornerMax) {
+				// then add points of both ends of the slice
 				arrayMin.push(pointA, pointB);
-				i = cornerMax; // jump around other polygon
+				// jump around to after the other end of the slice...
+				i = cornerMax;
 			} else {
+				// and add the remaining points
 				arrayMin.push(array[i]);
 			}
 			i++;
 		}
-		// add other polygon
+		// add 2nd polygon, a bit easier...
+		// start at point after the slice.
 		i = cornerMin + 1;
 		arrayMax.push(pointB, pointA);
 		while (i < cornerMax + 1) {
@@ -133,28 +141,70 @@ const reddit_proc_gen = function() {
 		return style;
 	};
 
+	let c = 0;
 	const drawAndSplit = (points, depth) => {
 		// split the shape
 		const polygons = splitPolygon(points);
 
-		// drawText(polyAlpha[0].x, polyAlpha[0].y, id++)
-		// drawPolygon(geom.insetPoints(polyBeta, border), {
-		// 	fillStyle: fillStyleBeta
-		// });
-		// drawText(polyBeta[0].x, polyBeta[0].y, id++)
-		// drawPolygon(polyBeta, {lineWidth: 1, strokeStyle: "#0f0"});
-
 		// recurse
 		depth++;
-		polygons.forEach((poly) => {
-			con.log("geom.polygonArea(poly)", geom.polygonArea(poly))
-			if (depth < maxDepth && (minArea === 0 || geom.polygonArea(poly) > minArea)) {
+		polygons.forEach(poly => {
+			if (
+				depth < maxDepth && // always honour max recursions else we crash...
+				(minArea === 0 || geom.polygonArea(poly) > minArea) // only honour minArea if not zero!
+			) {
 				drawAndSplit(poly, depth);
 			} else {
-				drawPolygon(
-					geom.insetPoints(poly, border),
-					getStyle()
-				);
+				const polyInset = geom.insetPoints(poly, border);
+
+				if (!polyInset) return;
+
+				// check if polygon is self intersecting, if so, discard it...
+				// yup, this is O(n^2).
+
+				const len = polyInset.length;
+				for (var j = 0; j < len - 1; j++) {
+					const indexA = j;
+					const indexB = j + 1;
+					const pointA = polyInset[indexA];
+					const pointB = polyInset[indexB];
+
+					// ctx.beginPath();
+					// ctx.strokeStyle = "red";
+					// ctx.lineWidth = 4;
+					// ctx.moveTo(pointA.x, pointA.y);
+					// ctx.lineTo(pointB.x, pointB.y);
+					// ctx.stroke();
+
+					for (var i = indexB; i < len; i++) {
+						c++;
+						const indexC = i;
+						const indexD = (i + 1) % len;
+						const pointC = polyInset[indexC];
+						const pointD = polyInset[indexD];
+						const intersects = geom.intersectionBetweenPoints(
+							pointA,
+							pointB,
+							pointC,
+							pointD
+						);
+						if (intersects) {
+							con.log("fail?");
+							return;
+						} else {
+							// con.log("ok");
+							// ctx.beginPath();
+							// ctx.strokeStyle = "#0f0";
+							// ctx.lineWidth = 8;
+							// ctx.moveTo(pointC.x, pointC.y);
+							// ctx.lineTo(pointD.x, pointD.y);
+							// ctx.stroke();
+							// con.log("intersects", intersects, pointA,pointB, pointC, pointD)
+						}
+					}
+				}
+
+				drawPolygon(polyInset, getStyle());
 			}
 		});
 	};
@@ -169,17 +219,25 @@ const reddit_proc_gen = function() {
 		cy = sh / 2;
 		ctx.clearRect(0, 0, sw, sh);
 
-		con.log(size);
+		const maxSize = size * size * 0.4;
+		minArea =
+			rand.getInteger(0, 1) === 0
+				? 0 // either 0, which means ignore minArea altogether
+				: rand.getInteger(maxSize * 0.05, maxSize); // or randomize it.
+		maxDepth = rand.getInteger(2, 8);
 
-		minArea = 160000;
-		maxDepth = 17;//rand.getInteger(2, 7);
-		border = -2;//-rand.getInteger(2, 8);
-		radius = sh * 0.5; // * rand.getNumber(0.3, 0.5);
+		// minArea = 0;
+		// maxDepth = 8;
+		con.log(minArea, maxDepth);
+
+		border = -rand.getInteger(2, 8);
+		radius = size * rand.getNumber(0.3, 0.5);
 		cutHalf = rand.getNumber(0, 1) > 0.2; // cutting in half is nice!
 
 		const points = [];
-		const sides = 4; //rand.getInteger(3, 7);
-		const startAngle = 0; //1 / 12 * TAU;//rand.getNumber(0, 1);
+		const sides = rand.getInteger(3, 7);
+		const startAngle = rand.getNumber(0, 1);
+		// const startAngle = 1 / 12 * TAU;
 		for (var i = 0; i < sides; i++) {
 			const a = startAngle + (i / sides) * -TAU;
 			const x = cx + Math.sin(a) * radius; // * 0.6,
@@ -195,13 +253,8 @@ const reddit_proc_gen = function() {
 		// inset the shape before recursion begins
 		drawAndSplit(geom.insetPoints(points, border), 0);
 		progress("render:complete", stage.canvas);
+		console.log("calculations", c);
 	};
-
-	function drawText(x, y, txt) {
-		ctx.font = "10px Helvetica";
-		ctx.fillStyle = "#fff";
-		ctx.fillText(txt, x, y);
-	}
 
 	return {
 		stage: stage.canvas,
