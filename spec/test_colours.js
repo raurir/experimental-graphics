8e77554b1caffd5f1a549a423784ddb79e958125
Rauri Rochford <raurir@gmail.com>
2019-02-11 23:01:58 +1100

    ok

--- spec/test_colours.js
+++ spec/test_colours.js
@@ -1,5 +1,5 @@
 var rand = require("../src/rand.js");
-var colours = require("../src/colours.js").instance(rand);
+var coloursGlobal = require("../src/colours.js");
 
 describe("colours", () => {
 	// untested methods:
@@ -7,9 +7,12 @@ describe("colours", () => {
 	// setColourIndex - not a popular method
 	// show* - for showcasing colours, no point
 
+	// all tests were written on colours v1:
+	var colours = coloursGlobal.instance(rand, null, "v1");
+
 	describe("getPalette", () => {
 		it("should return the current palette", () => {
-			var expectedPalette = ["#A0C9D9", "#F2F0D0", "#735438", "#A64941", "#0D0D0D"];
+			var expectedPalette = ["#a0c9d9", "#f2f0d0", "#735438", "#a64941", "#0d0d0d"];
 			rand.setSeed(89733);
 			expect(colours.getRandomPalette()).toEqual(expectedPalette);
 			expect(colours.getPalette()).toEqual(expectedPalette);
@@ -19,53 +22,53 @@ describe("colours", () => {
 	describe("getRandomPalette", () => {
 		it("should return a deterministic palette", () => {
 			rand.setSeed(1234);
-			expect(colours.getRandomPalette()).toEqual(["#9D7E79", "#CCAC95", "#9A947C", "#748B83", "#5B756C"]);
+			expect(colours.getRandomPalette()).toEqual(["#9d7e79", "#ccac95", "#9a947c", "#748b83", "#5b756c"]);
 			rand.setSeed(56789);
-			expect(colours.getRandomPalette()).toEqual(["#00A0B0", "#6A4A3C", "#CC333F", "#EB6841", "#EDC951"]);
+			expect(colours.getRandomPalette()).toEqual(["#00a0b0", "#6a4a3c", "#cc333f", "#eb6841", "#edc951"]);
 		});
 	});
 
 	describe("getRandomColour", () => {
 		it("should return a deterministic colour", () => {
 			rand.setSeed(87652);
-			expect(colours.getRandomPalette()).toEqual(["#556270", "#4ECDC4", "#C7F464", "#FF6B6B", "#C44D58"]);
-			expect(colours.getRandomColour()).toBe("#FF6B6B");
-			expect(colours.getRandomColour()).toBe("#C44D58");
+			expect(colours.getRandomPalette()).toEqual(["#556270", "#4ecdc4", "#c7f464", "#ff6b6b", "#c44d58"]);
+			expect(colours.getRandomColour()).toBe("#ff6b6b");
+			expect(colours.getRandomColour()).toBe("#c44d58");
 		});
 	});
 
 	describe("getCurrentColour", () => {
 		it("should return the current colour", () => {
 			rand.setSeed(98319);
-			expect(colours.getRandomPalette()).toEqual(["#00A8C6", "#40C0CB", "#F9F2E7", "#AEE239", "#8FBE00"]);
-			expect(colours.getCurrentColour()).toBe("#8FBE00");
-			expect(colours.getCurrentColour()).toBe("#8FBE00");
+			expect(colours.getRandomPalette()).toEqual(["#00a8c6", "#40c0cb", "#f9f2e7", "#aee239", "#8fbe00"]);
+			expect(colours.getCurrentColour()).toBe("#8fbe00");
+			expect(colours.getCurrentColour()).toBe("#8fbe00");
 		});
 	});
 
 	describe("getNextColour", () => {
 		it("should return the next colour", () => {
 			rand.setSeed(1289371);
-			expect(colours.getRandomPalette()).toEqual(["#4E4D4A", "#353432", "#94BA65", "#2790B0", "#2B4E72"]);
-			expect(colours.getRandomColour()).toBe("#4E4D4A");
+			expect(colours.getRandomPalette()).toEqual(["#4e4d4a", "#353432", "#94ba65", "#2790b0", "#2b4e72"]);
+			expect(colours.getRandomColour()).toBe("#4e4d4a");
 			expect(colours.getNextColour()).toBe("#353432");
-			expect(colours.getNextColour()).toBe("#94BA65");
-			expect(colours.getNextColour()).toBe("#2790B0");
-			expect(colours.getNextColour()).toBe("#2B4E72");
-			expect(colours.getNextColour()).toBe("#4E4D4A");
+			expect(colours.getNextColour()).toBe("#94ba65");
+			expect(colours.getNextColour()).toBe("#2790b0");
+			expect(colours.getNextColour()).toBe("#2b4e72");
+			expect(colours.getNextColour()).toBe("#4e4d4a");
 		});
 	});
 
 	describe("setPalette & setPaletteRange", () => {
 		it("should accept and handle an externally defined palette", () => {
 			rand.setSeed(5);
-			colours.setPalette(["#110088", "#22FF44", "#3300FF"]);
+			colours.setPalette(["#110088", "#22ff44", "#3300ff"]);
 			expect(colours.getRandomColour()).toBe("#110088");
-			expect(colours.getNextColour()).toBe("#22FF44");
+			expect(colours.getNextColour()).toBe("#22ff44");
 
 			colours.setPaletteRange(2);
 			expect(colours.getCurrentColour()).toEqual("#110088");
-			expect(colours.getNextColour()).toEqual("#22FF44");
+			expect(colours.getNextColour()).toEqual("#22ff44");
 			expect(colours.getNextColour()).toEqual("#110088");
 		});
 	});
@@ -73,33 +76,33 @@ describe("colours", () => {
 	describe("setRandomPalette", () => {
 		it("should set a palette by index", () => {
 			rand.setSeed(500);
-			expect(colours.getRandomPalette()).toEqual(["#1B676B", "#519548", "#88C425", "#BEF202", "#EAFDE6"]);
+			expect(colours.getRandomPalette()).toEqual(["#1b676b", "#519548", "#88c425", "#bef202", "#eafde6"]);
 			colours.setRandomPalette(4);
-			expect(colours.getPalette()).toEqual(["#1B69FF", "#002875", "#0143C2", "#FFB002", "#FF781E"]);
+			expect(colours.getPalette()).toEqual(["#1b69ff", "#002875", "#0143c2", "#ffb002", "#ff781e"]);
 		});
 	});
 
 	describe("mutateColour", () => {
 		it("should mutate a colour by the specified amount", () => {
 			rand.setSeed(500);
-			expect(colours.mutateColour("#1B69FF", 2)).toBe("#1b69fe");
-			expect(colours.mutateColour("#1B69FF", 20)).toBe("#156bff");
-			expect(colours.mutateColour("#1B69FF", 200)).toBe("#6394ff");
-			expect(colours.mutateColour("#1B69FF", 2000)).toBe("#00ff00");
+			expect(colours.mutateColour("#1b69ff", 2)).toBe("#1b69fe");
+			expect(colours.mutateColour("#1b69ff", 20)).toBe("#156bff");
+			expect(colours.mutateColour("#1b69ff", 200)).toBe("#6394ff");
+			expect(colours.mutateColour("#1b69ff", 2000)).toBe("#00ff00");
 		});
 	});
 
 	describe("mixColours", () => {
 		it("should mix a set of colours to find the average", () => {
-			expect(colours.mixColours(["#FF00FF", "#00FF00"])).toBe("#7f7f7f");
-			expect(colours.mixColours(["#AA0000", "#FF0000"])).toBe("#d40000");
+			expect(colours.mixColours(["#ff00ff", "#00ff00"])).toBe("#7f7f7f");
+			expect(colours.mixColours(["#aa0000", "#ff0000"])).toBe("#d40000");
 			expect(colours.mixColours(["#000000", "#111111", "#222222", "#333333", "#444444"])).toBe("#222222");
 			rand.setSeed(200);
 			expect(colours.mixColours(colours.getRandomPalette())).toBe("#bb8277");
 		});
 	});
 
-	describe("versions", () => {
+	describe("with multiple versions", () => {
 		var colV0, colV1, colV2;
 		beforeEach(() => {
 			var randA = rand.instance(200);
@@ -112,24 +115,35 @@ describe("colours", () => {
 			colV2 = colours.instance(randC, null, "v2"); // this version doesn't exist right now!
 		});
 
-		describe("with different versions", () => {
-			it("should not have the same random colours", () => {
-				colV0.getRandomPalette();
-				colV1.getRandomPalette();
-				colV2.getRandomPalette();
-				expect(colV0.getNextColour()).not.toEqual(colV1.getNextColour());
-				expect(colV0.getNextColour()).not.toEqual(colV2.getNextColour());
-			});
-
-			it("if setRandomPalette is within range, should have the same palette", () => {
-				// all versions have palette index 2
-				colV0.setRandomPalette(2);
-				colV1.setRandomPalette(2);
-				colV2.setRandomPalette(2);
-				expect(colV0.getNextColour()).toEqual(colV1.getNextColour());
-				colV2.getNextColour(); // otherwiser colV1 would be ahead s
-				expect(colV1.getNextColour()).toEqual(colV2.getNextColour());
-			});
+		it("should not have the same random colours", () => {
+			colV0.getRandomPalette();
+			colV1.getRandomPalette();
+			colV2.getRandomPalette();
+			expect(colV0.getNextColour()).not.toEqual(colV1.getNextColour());
+			expect(colV0.getNextColour()).not.toEqual(colV2.getNextColour());
 		});
+
+		it("if setRandomPalette is within range, should have the same palette", () => {
+			// all versions have palette index 2
+			colV0.setRandomPalette(2);
+			colV1.setRandomPalette(2);
+			colV2.setRandomPalette(2);
+			expect(colV0.getNextColour()).toEqual(colV1.getNextColour());
+			colV2.getNextColour(); // otherwiser colV1 would be ahead s
+			expect(colV1.getNextColour()).toEqual(colV2.getNextColour());
+		});
+	});
+
+	describe("with v2", () => {
+		// everything else should behave as normal but, let's find a seed that gives a palette outside v1's range...
+		// turns out 20000 works.
+		var r = rand.instance(20000);
+		var colours = coloursGlobal.instance(r, null, "v2");
+
+		colours.getRandomPalette();
+
+		expect(colours.getPalleteIndex()).toBe(417); // 417 is way outside v1's range of 121!
+		expect(colours.getPalette()).toEqual(["#fec998", "#76c9a3", "#423a3b", "#f8f0a3", "#b9c99a"]);
+		expect(colours.getNextColour()).toEqual("#76c9a3");
 	});
 });
