228b6354a0c1cbc68a0dbfffbd309a66dff0b237
Rauri Rochford <raurir@gmail.com>
2019-03-06 21:37:02 +1100

    meh

--- src/perlin_noise.js
+++ src/perlin_noise.js
@@ -20,6 +20,12 @@ var perlin_noise = function(perlin) {
 	// var min = 1000;
 	// var max = -1000;
 
+	const genClamp = (min, max) => (val) => {
+		if (val < min) return min;
+		if (val > max) return max;
+		return val;
+	};
+
 	function drawColours(time) {
 		var t = time * 0.005;
 		var scale = 0.01;
@@ -51,34 +57,26 @@ var perlin_noise = function(perlin) {
 		requestAnimationFrame(drawColours);
 	}
 
+	const c01 = genClamp(0, 1);
+
 	function drawCircles(time) {
 		d.clearRect(0, 0, w * pixel, h * pixel);
 		var t = time * 0.005;
-		var scale = 0.01;
-		var red = channelRed.cycle(t, scale);
-		var green = channelGreen.cycle(t, scale);
-		var blue = channelBlue.cycle(t, scale);
+		var white = channelRed.cycle(t * 0.11, 0.016);
+		var blue = channelBlue.cycle(t * 0.7, 0.009);
 
 		for (var i = 0, il = w * h; i < il; i++) {
 			var xp = i % w;
 			var yp = Math.floor(i / w);
-			// d.save();
-			// d.setTransform( 1, 0, 0, 1, 0, 0 );
-			// var r = ~~(red[i] * 255),
-			// 	g = ~~(green[i] * 255),
-			// 	b = ~~(blue[i] * 255);
-			// d.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-			d.fillStyle = "#000";
+			d.fillStyle = "rgba(20,50,120,1)";
 			d.beginPath();
-			// d.moveTo(xp, yp);
-			d.drawCircle(xp * pixel, yp * pixel, (red[i] * pixel) / 2);
+			d.drawCircle(xp * pixel, yp * pixel, (c01(white[i]) * pixel) / 2);
 			d.closePath();
 			d.fill();
 
-			d.fillStyle = "#00a";
+			d.fillStyle = "rgba(200,245,255,1)";
 			d.beginPath();
-			// d.moveTo(xp, yp);
-			d.drawCircle((xp + 0.5) * pixel, yp * pixel, (blue[i] * pixel) / 4);
+			d.drawCircle(xp * pixel, yp * pixel, (c01(blue[i]) * pixel) / 2);
 			d.closePath();
 			d.fill();
 		}
