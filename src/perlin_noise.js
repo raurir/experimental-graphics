fd5f26826cba84794293bf5a7ced06e16d1a5987
raurir <raurir@gmail.com>
2019-03-06 16:59:10 +1100

    autoformat

--- src/perlin_noise.js
+++ src/perlin_noise.js
@@ -1,65 +1,64 @@
 var perlin_noise = function(perlin) {
+	const pixel = 10;
+	const w = 60;
+	const h = 60;
 
-  var timer=((c)=>{var t={},f=()=>new Date().getTime();return {start:(k)=>{t[k]=f();},end:(k)=>{c.log(k,f()-t[k]);}};})(console);
+	const M = Math;
+	// const r = M.random;
 
-  const pixel = 10;
-  const w = 60;
-  const h = 60;
+	const c = document.createElement("canvas");
+	c.width = w * pixel;
+	c.height = h * pixel;
+	const d = c.getContext("2d");
 
-  const M = Math;
-  const r = M.random;
+	const logger = document.createElement("div");
+	document.body.appendChild(logger);
 
-  const c = document.createElement("canvas");
-  c.width = w * pixel;
-  c.height = h * pixel;
-  const d = c.getContext('2d');
+	// var iterations = 0;
+	var channelRed = perlin.noise(w, h);
+	var channelGreen = perlin.noise(w, h);
+	var channelBlue = perlin.noise(w, h);
 
-  const logger = document.createElement("div");
-  document.body.appendChild(logger);
+	var min = 1000;
+	var max = -1000;
 
-  var iterations = 0;
-  var channelRed = perlin.noise(w, h);
-  var channelGreen = perlin.noise(w, h);
-  var channelBlue = perlin.noise(w, h);
+	function drawIt(time) {
+		var t = time * 0.005;
+		var scale = 0.01;
+		var red = channelRed.cycle(t, scale);
+		var green = channelGreen.cycle(t, scale);
+		var blue = channelBlue.cycle(t, scale);
 
-  var min = 1000;
-  var max = -1000;
+		for (var i = 0, il = w * h; i < il; i++) {
+			var xp = i % w;
+			var yp = Math.floor(i / w);
+			// d.save();
+			// d.setTransform( 1, 0, 0, 1, 0, 0 );
+			var r = ~~(red[i] * 255),
+				g = ~~(green[i] * 255),
+				b = ~~(blue[i] * 255);
+			d.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+			d.fillRect(xp * pixel, yp * pixel, pixel, pixel);
 
-  function drawIt(time) {
-    var t = time * 0.005;
-    var scale = 0.01;
-    var red = channelRed.cycle(t, scale);
-    var green = channelGreen.cycle(t, scale);
-    var blue = channelBlue.cycle(t, scale);
+			min = M.min(r, min);
+			max = M.max(r, max);
+			min = M.min(g, min);
+			max = M.max(g, max);
+			min = M.min(b, min);
+			max = M.max(b, max);
+		}
 
-    for (var i = 0, il = w * h; i < il; i++){
-      var xp = i % w;
-      var yp = Math.floor(i / w);
-      // d.save();
-      // d.setTransform( 1, 0, 0, 1, 0, 0 );
-      var r = ~~(red[i] * 255),
-        g = ~~(green[i] * 255),
-        b = ~~(blue[i] * 255);
-      d.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-      d.fillRect(xp * pixel, yp * pixel, pixel, pixel);
+		logger.innerHTML = min + "<br>" + max;
 
-      min = M.min(r, min); max = M.max(r, max);
-      min = M.min(g, min); max = M.max(g, max);
-      min = M.min(b, min); max = M.max(b, max);
-    }
+		requestAnimationFrame(drawIt);
+	}
 
-    logger.innerHTML = min + '<br>' +  max;
-
-    requestAnimationFrame(drawIt);
-  };
-
-  return {
-    init: () => {
-      drawIt(0);
-    },
-    stage: c
-  };
-
+	return {
+		init: () => {
+			drawIt(0);
+		},
+		stage: c,
+	};
 };
 
 // define("perlin_noise", perlin_noise);
